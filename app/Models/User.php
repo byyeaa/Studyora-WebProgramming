2173231d59d43322bf84b30df7097f83a49eef06
Beatrix Claudya Sihombing <145783997+byyeaa@users.noreply.github.com>
2025-12-27 21:55:21 +0700

    Update User.php

--- app/Models/User.php
+++ app/Models/User.php
@@ -33,51 +33,6 @@ class User extends Authenticatable implements MustVerifyEmail
         ];
     }
 
-    public function sendEmailVerificationNotification()
-    {
-        $verificationUrl = URL::temporarySignedRoute(
-            'verification.verify',
-            Carbon::now()->addMinutes(60),
-            [
-                'id' => $this->getKey(),
-                'hash' => sha1($this->getEmailForVerification()),
-            ]
-        );
-
-       $response = Http::withHeaders([
-        'api-key' => config('services.brevo.key'),
-        'accept' => 'application/json',
-        'content-type' => 'application/json',
-        ])->post('https://api.brevo.com/v3/smtp/email', [
-        'sender' => [
-            'email' => 'no-reply@brevo.com',
-            'name'  => 'Studyora',
-        ],
-            'to' => [
-                [
-                    'email' => $this->email,
-                    'name'  => $this->name,
-                ],
-            ],
-            'subject' => 'Verifikasi Email Akun Studyora',
-            'htmlContent' => "
-                <p>Halo <b>{$this->name}</b>,</p>
-                <p>Silakan klik tombol berikut untuk verifikasi email kamu:</p>
-                <p>
-                    <a href='{$verificationUrl}'
-                       style='display:inline-block;
-                              padding:12px 20px;
-                              background:#4f46e5;
-                              color:#ffffff;
-                              text-decoration:none;
-                              border-radius:6px'>
-                        Verifikasi Email
-                    </a>
-                </p>
-                <p>Jika bukan kamu, abaikan email ini.</p>
-            ",
-        ]);
-    }
 
     public function getTotalPointsAttribute()
     {
