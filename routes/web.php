5835911629efab4fe242661cbe306e51d5b89549
Beatrix Claudya Sihombing <145783997+byyeaa@users.noreply.github.com>
2025-12-29 10:14:13 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -36,7 +36,6 @@ Route::get('/', function () {
 
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::patch('/profile/photo', [ProfileController::class, 'updatePhoto'])->name('profile.photo.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
