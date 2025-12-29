64fb965d757f4909d4dcf22ea5e9e056708eac9d
Beatrix Claudya Sihombing <145783997+byyeaa@users.noreply.github.com>
2025-12-29 10:04:43 +0700

    Add route for updating profile photo

--- routes/web.php
+++ routes/web.php
@@ -36,6 +36,7 @@ Route::get('/', function () {
 
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::patch('/profile/photo', [ProfileController::class, 'updatePhoto'])->name('profile.photo.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
