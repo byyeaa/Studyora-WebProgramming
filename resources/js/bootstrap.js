bbf86c5c3dd168b40c76dedd931d0726a2cd3969
Beatrix Claudya Sihombing <145783997+byyeaa@users.noreply.github.com>
2025-12-29 22:44:20 +0700

    Update bootstrap.js

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,6 +1,3 @@
-// Axios
 import axios from 'axios';
 window.axios = axios;
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
-
-import 'bootstrap/dist/js/bootstrap.bundle.min.js';
