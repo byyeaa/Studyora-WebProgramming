9ac3517a52e0c66103f72a6a4cc17f4351994d54
Beatrix Claudya Sihombing <145783997+byyeaa@users.noreply.github.com>
2025-12-30 15:30:01 +0700

    Update update-profile-information-form.blade.php

--- resources/views/profile/partials/update-profile-information-form.blade.php
+++ resources/views/profile/partials/update-profile-information-form.blade.php
@@ -28,7 +28,7 @@
             <x-text-input id="email" name="email" type="email" class="mt-1 block w-full" :value="old('email', $user->email)" required autocomplete="username" />
             <x-input-error class="mt-2" :messages="$errors->get('email')" />
 
-            @if ($user instanceof \Illuminate\Contracts\Auth\MustVerifyEmail && ! $user->hasVerifiedEmail())
+            <!-- @if ($user instanceof \Illuminate\Contracts\Auth\MustVerifyEmail && ! $user->hasVerifiedEmail())
                 <div>
                     <p class="text-sm mt-2 text-gray-800">
                         {{ __('Alamat email Anda belum diverifikasi.') }}
@@ -45,7 +45,7 @@
                     @endif
                 </div>
             @endif
-        </div>
+        </div> -->
 
         <div class="flex items-center gap-4">
             <x-primary-button>{{ __('Simpan') }}</x-primary-button>
