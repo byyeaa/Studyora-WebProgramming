571b6c0974752c5b023f1e5b872024b302b14313
Beatrix Claudya Sihombing <145783997+byyeaa@users.noreply.github.com>
2025-12-30 15:39:27 +0700

    Update leaderboard.blade.php

--- resources/views/leaderboard.blade.php
+++ resources/views/leaderboard.blade.php
@@ -52,7 +52,7 @@
             <div class="card p-3 mb-3">
                 <div class="d-flex justify-content-between align-items-center">
                     <div>
-                        <h5 class="fw-bold mb-0">{{ $index + 4 }}. {{ $user->name }}</h5>
+                        <h5 class="fw-bold mb-0">{{ $index + 1 }}. {{ $user->name }}</h5>
                         <small class="text-muted">{{ $user->final_points }} Points</small>
                     </div>
                     <i class="bi bi-trophy-fill" style="font-size:40px; color:lightgray;"></i>
